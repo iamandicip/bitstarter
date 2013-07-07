22b2081f2c909521d58e45e5ae5a828a12490bd5
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2013-07-07 23:17:38 +0000

    fixed some bugs

--- web.js
+++ web.js
@@ -3,23 +3,31 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 var buffer = new Buffer("Hello world!");
-
-fs.readFile("./index.html", "utf-8", function (err, data){
-    if(err){
-	console.log("error reading file index.html");
-    }else{
-    	buffer.write(data.toString(), 0, data.toString().length, "utf-8");
-    }
-});
-
-
-console.log("read from buffer: " + buffer.toString("utf-8"));
+var filename = "./index.html";
+var data = buffer.toString("utf8", 0, buffer.length);
 
 app.get('/', function(request, response) {
-  response.send(buffer.toString("utf-8", 0, buffer.length));
+	response.send(
+		fs.exists(filename, function(exists){
+			if(exists){
+				fs.stat(filename, function(error, stats){
+					fs.open(filename, "r", function(error, fd){
+						buffer = new Buffer(stats.size);
+						fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer){
+							data = buffer.toString("utf8", 0, buffer.length);
+							console.log(data);
+							fs.close(fd);
+							return data;
+						});
+					});
+				});
+
+			}
+		})
+		);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
