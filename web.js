0bfd1cbdbf966de0529214fb426222928c1d37d0
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2013-07-07 21:37:09 +0000

    addded reading from index.html

--- web.js
+++ web.js
@@ -1,9 +1,22 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var buffer = new Buffer("Hello world!");
+
+fs.readFileSync("index.html", function (err, data){
+    if(err){
+	console.log("error reading file index.html");
+    }else{
+	buffer.write(data, 0, data.length, "utf-8");
+    }
+});
+
+
+console.log("read from buffer: " + buffer.toString());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
