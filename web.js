4263807ab9fe28ac857b2dcda470f9a809514275
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2013-07-08 09:00:27 +0000

    some more changes

--- web.js
+++ web.js
@@ -7,26 +7,26 @@ var filename = "./index.html";
 var data = buffer.toString("utf8", 0, buffer.length);
 
 fs.exists(filename, function(exists){
-	if(exists){
-		fs.stat(filename, function(error, stats){
-			fs.open(filename, "r", function(error, fd){
-				buffer = new Buffer(stats.size);
-				fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer){
-					data = buffer.toString("utf8", 0, buffer.length);
-					fs.close(fd);
-				});
-			});
-		});
+    if(exists){
+        fs.stat(filename, function(error, stats){
+            fs.open(filename, "r", function(error, fd){
+                buffer = new Buffer(stats.size);
+                fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer){
+                    data = buffer.toString("utf8", 0, buffer.length);
+                    fs.close(fd);
+                });
+            });
+        });
 
-	}
+    }
 });
 
 app.get('/', function(request, response) {
-	console.log(data);
-	response.send(data);
+    console.log(data);
+    response.send(data);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-	console.log("Listening on " + port);
+    console.log("Listening on " + port);
 });
