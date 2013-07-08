a8bf29e54f46d985d94077b1db4746d7f1b68afe
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2013-07-08 07:49:36 +0000

    some more changes in web.js

--- web.js
+++ web.js
@@ -6,25 +6,24 @@ var buffer = new Buffer("Hello world!");
 var filename = "./index.html";
 var data = buffer.toString("utf8", 0, buffer.length);
 
-app.get('/', function(request, response) {
-	response.send(
-		fs.exists(filename, function(exists){
-			if(exists){
-				fs.stat(filename, function(error, stats){
-					fs.open(filename, "r", function(error, fd){
-						buffer = new Buffer(stats.size);
-						fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer){
-							data = buffer.toString("utf8", 0, buffer.length);
-							console.log(data);
-							fs.close(fd);
-							return data;
-						});
-					});
+fs.exists(filename, function(exists){
+	if(exists){
+		fs.stat(filename, function(error, stats){
+			fs.open(filename, "r", function(error, fd){
+				buffer = new Buffer(stats.size);
+				fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer){
+					data = buffer.toString("utf8", 0, buffer.length);
+					fs.close(fd);
 				});
+			});
+		});
 
-			}
-		})
-		);
+	}
+});
+
+app.get('/', function(request, response) {
+	console.log(data);
+	response.send(data);
 });
 
 var port = process.env.PORT || 5000;
