08b8122c47fd709a72f8161cd7364dc741b62081
Ciprian Iamandi <ciprian.iamandi@gmail.com>
2013-07-17 22:26:48 +0000

    Added styles in index.html

--- web.js
+++ web.js
@@ -7,26 +7,25 @@ var filename = "./index.html";
 var data = buffer.toString("utf8", 0, buffer.length);
 
 fs.exists(filename, function(exists){
-    if(exists){
-        fs.stat(filename, function(error, stats){
-            fs.open(filename, "r", function(error, fd){
-                buffer = new Buffer(stats.size);
-                fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer){
-                    data = buffer.toString("utf8", 0, buffer.length);
-                    fs.close(fd);
-                });
-            });
-        });
+	if(exists){
+		fs.stat(filename, function(error, stats){
+			fs.open(filename, "r", function(error, fd){
+				buffer = new Buffer(stats.size);
+				fs.read(fd, buffer, 0, buffer.length, null, function(error, bytesRead, buffer){
+					data = buffer.toString("utf8", 0, buffer.length);
+					fs.close(fd);
+				});
+			});
+		});
 
-    }
+	}
 });
 
 app.get('/', function(request, response) {
-    console.log(data);
-    response.send(data);
+	response.send(data);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
-    console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
